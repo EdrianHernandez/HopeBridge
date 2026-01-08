40909f46348c4f93db8ae8773f035aabb1a96085
Edrian Hernandez <117151322+EdrianHernandez@users.noreply.github.com>
2026-01-08 22:59:41 +0800

    Refactor Footer component to remove TypeScript type

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,6 @@
-
 import React from 'react';
 
-const Footer: React.FC = () => {
+const Footer = () => {
   return (
     <footer className="bg-slate-900 text-slate-300 py-20">
       <div className="container mx-auto px-4">
