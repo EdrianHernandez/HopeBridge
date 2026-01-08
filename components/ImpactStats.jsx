cd0b1bb267a00d393c45f27a4a5d2a5db417a2d3
Edrian Hernandez <117151322+EdrianHernandez@users.noreply.github.com>
2026-01-08 23:01:15 +0800

    Refactor ImpactStats component to use implicit return

--- components/ImpactStats.jsx
+++ components/ImpactStats.jsx
@@ -1,8 +1,7 @@
-
 import React from 'react';
 import { IMPACT_STATS } from '../constants';
 
-const ImpactStats: React.FC = () => {
+const ImpactStats = () => {
   return (
     <div className="container mx-auto px-4">
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
